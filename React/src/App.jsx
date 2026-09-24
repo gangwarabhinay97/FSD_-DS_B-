69e5fa4106b271a59e860ec9f358f67af2fd922c
gangwarabhinay97 <gangwarabhinay97@gmail.com>
2026-09-24 10:21:56 +0530

    route

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,11 +4,11 @@ import Id from './Component/Id'
 const App = () => {
   return (
     <div style={{ display:'flex' , gap:'10px'}}>
-      <Id/>
+      <Id name="Abhinay Gangwar" class="B.tech" rollNumber="001" image="https://media.licdn.com/dms/image/v2/D5635AQEQHtVORJe0og/profile-framedphoto-shrink_200_200/B56Z6OjF42KYAQ-/0/1780508024891?e=1790830800&v=beta&t=tmIJHBJVrlL6GktHHaykJ1g1e6RpB_gfmMDsIQQ9nLQ"/>
       <br />
-       <Id/>
+       <Id name="Akshay Dixhit" class="MBA" rollNumber="002" image="https://media.licdn.com/dms/image/v2/D5635AQEF8k016nN9Jw/profile-framedphoto-shrink_800_800/B56Z2QYLzCGgAg-/0/1776243752603?e=1790830800&v=beta&t=XxyKIOq3k4E35pfRxIwPE23iSB8AZqqX9N45wALvchY"/>
       <br />
-       <Id/>
+       <Id name="Ansh Grewal" class="8th" rollNumber="003" image="https://media.licdn.com/dms/image/v2/D4D03AQG5dUGSjJ5m3g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1698607912702?e=1792022400&v=beta&t=I_w2uV1PZyE-UTAAYh3knsNm7xeEbY4kG9dfFfKcKZ8" />
       <br />
     </div>
   )
--- React/src/Component/Id.jsx
+++ React/src/Component/Id.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const Id = () => {
+const Id = (props) => {
   return (
     <div style={{ border: '2px solid red' , height:'400px' , width:'300px' }}>
+        <h4>ABES ENGINEERING COLLEGE</h4>
         <h3>Student Id</h3>
-        <h3>Abhinay Gangwar</h3>
-        <img src="C:\Users\abhin\Downloads\image abhinay.jpg" alt="" height={'100px'} width={'100px'} />
-        <h3>Roll Number: 12345</h3>
-        <h3>Class: B.tech</h3>
+        <h3>{props.name}</h3>
+        <img src={props.image} alt="" height={'100px'} width={'100px'} />
+        <h3>{props.rollNumber}</h3>
+        <h3>{props.class}</h3>
     </div>
   )
 }
